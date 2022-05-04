d84fcaa4e882b44a3cd94d2181d927326307d0ab
Nicolás Ríos <nicolas.rios@doublevpartners.com>
2022-05-04 16:19:41 -0500

    Deleted not existing modules and components

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cv'),
+      dir: require('path').join(__dirname, './coverage/pwa-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => // eslint-disable-line @typescript-eslint/explicit-function-return-type
-      import('./modules/home/home.module').then((m) => m.HomeModule), // eslint-disable-line @typescript-eslint/typedef
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
